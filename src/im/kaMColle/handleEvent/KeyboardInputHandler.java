f1cd12bf11d672a7c989a48e06725995de23137a
kongren <kongren@kongren-PC>
2015-07-12 22:34:02 +0800

    模型修改

--- src/im/kaMColle/handleEvent/KeyboardInputHandler.java
+++ src/im/kaMColle/handleEvent/KeyboardInputHandler.java
@@ -3,8 +3,10 @@ package im.kaMColle.handleEvent;
 import im.kaMColle.Kamcolle;
 import im.kaMColle.network.MessageHandler;
 import im.kaMColle.network.packet.KansouControlMessage;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
 
@@ -12,21 +14,25 @@ public class KeyboardInputHandler {
 	byte t=0;
 	static final byte MAX_V=10;
 	EntityPlayer me=Minecraft.getMinecraft().thePlayer;
+	World mw=Minecraft.getMinecraft().theWorld;
 	@SubscribeEvent
 	public void listenKeyJumpAndSneak(KeyInputEvent e){
-		if(Minecraft.getMinecraft().gameSettings.keyBindJump.getIsKeyPressed()){
-			t+=1;
+		if(mw.isAABBInMaterial(me.boundingBox, Material.water)){
+			if(Minecraft.getMinecraft().gameSettings.keyBindJump.getIsKeyPressed()){
+				addFlow(1);
+			}
+			if(Minecraft.getMinecraft().gameSettings.keyBindSneak.getIsKeyPressed()){
+				addFlow(-1); 
+			}
 		}
-		if(Minecraft.getMinecraft().gameSettings.keyBindSneak.getIsKeyPressed()){
-			t-=1; 
-		}
+		
 		Kamcolle.LogInfo(t);
 		MessageHandler.INSTANCE.sendToServer(new KansouControlMessage(Minecraft.getMinecraft().thePlayer,t));
 	}
 	private int addFlow(int i){
 		t+=i;
-		if(t>0)t=(byte) Math.min(MAX_V, t);
-		else if(t<0)Math.max(-MAX_V, t);
+		if(t>MAX_V)t=MAX_V;
+		else if(t<-MAX_V)t=-MAX_V;
 		return t;
 	}
 }
